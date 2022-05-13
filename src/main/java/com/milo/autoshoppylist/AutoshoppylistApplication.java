2d5069d5311f129b414552678fb80df29c7ca03a
sosnowskimilosz <sosnowskimilosz@gmail.com>
2022-05-13 15:55:25 +0200

    initial commit with files

---
+++ src/main/java/com/milo/autoshoppylist/AutoshoppylistApplication.java
@@ -0,0 +1,13 @@
+package com.milo.autoshoppylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoshoppylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoshoppylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milo/autoshoppylist/AutoshoppylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.autoshoppylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoshoppylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
